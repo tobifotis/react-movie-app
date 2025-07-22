eff80e37ad40da5c81fe005d203b7cffb4c9a932
tobifotis <perspectiveguy35@gmail.com>
2025-07-22 16:02:19 -0500

    feat(navbar): add navigation bar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css'
 import Home from "./pages/Home"
 import Favorites from "./pages/Favorites"
+import NavBar from './components/NavBar'
 import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
+    <div>
+      <NavBar />
+      <main className = "main-content">
+        <Routes>
 
-    <main className = "main-content">
-      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
 
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites />} />
-
-      </Routes>
-    </main>
+        </Routes>
+      </main>
+    </div>
 
   )
 }
