6fc602c6f21d0c65bb48ab73efe3e7f68c549283
tobifotis <perspectiveguy35@gmail.com>
2025-07-23 02:35:21 -0500

    feat(API): fetch data and movie posters from 'themoviedb.org'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './css/App.css'
 import Home from "./pages/Home"
 import Favorites from "./pages/Favorites"
 import NavBar from './components/NavBar'
---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+import "../css/MovieCard.css"
+
+// Movie object 
+function MovieCard({movie}) {
+
+    function onFavoriteClick() {
+        alert("clicked")
+    }
+
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>
+                        ♥
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date?.split("-")[0]}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
